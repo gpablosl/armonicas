9b749bfe967f00cc7c389b91404f1d7b597bfb37
gpablosl <69810373+gpablosl@users.noreply.github.com>
2022-02-28 10:48:03 -0700

    Update armoicas.py

--- armoicas.py
+++ armoicas.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
 frecuencia_muestreo = 44100
-frecuencia = 250
+frecuencia = 500
 tiempos = np.linspace(0.0,1.0,frecuencia_muestreo)
 amplitud = np.iinfo(np.int16).max
 
@@ -34,4 +34,26 @@ frecuencias = np.fft.rfftfreq(cantidad_muestras, periodo_muestreo)
 
 plt.figure()
 plt.plot(frecuencias, np.abs(transformada))
+#plt.show()
+
+
+#1 obtener en Hz las frecuencias de  los armónicos de la señal
+
+print(frecuencias[transformada > 100000])
+
+#2 aplicar filtro pasabajas que solo deje pasar la frecuencia fundamental y aplicar transformada inversa
+#graficar y crear archivo wav 
+pasa_bajas = transformada.copy()
+pasa_bajas[frecuencias > frecuencia] *= 0
+
+plt.plot(frecuencias, np.abs(pasa_bajas), label = "Espectro filtrado, pasa bajas")
+plt.legend()
 plt.show()
+
+pasa_bajas_data = np.fft.irfft(pasa_bajas)
+
+plt.plot(tiempos,pasa_bajas_data, label="Audio con pasa bajas")
+plt.legend()
+plt.show()
+
+write("triangular_bajas.wav", frecuencia_muestreo, pasa_bajas_data.astype(np.int16))
